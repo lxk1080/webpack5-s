7ddf1554ced451e357104ae9bf4287ea007bb3f5
zerol <2664276518@qq.com>
2024-04-07 20:05:21 +0800

    chore: update desc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   extends: ["eslint:recommended"],
   // 解决 webpack import 语法报错问题，需要安装 eslint-plugin-import 插件库
   // 需要同时安装 babel-eslint@8，注意版本高了会报错，并在下面配置为解析器：parser: "babel-eslint"
-  // 另外：配置下面的 ecmaVersion: 11 也可解决问题，但这个是利用 es11 的 import 特性，并不是真的 webpack 的 import() 语法
+  // 另外：
+  // 配置下面的 ecmaVersion: 11 也可解决问题，但这个是利用 es11 的 import 特性，并不是真的 webpack 提供的 import() 语法（但其实在使用上也没差，而且这个更方便）
+  // 这里是为了演示，所以使用的是 eslint-plugin-import 插件，实际工作中，推荐 ecmaVersion: 11 这种方式解决
   plugins: ["import"],
   env: {
     node: true, // 启用 node 中全局变量，例如：console
